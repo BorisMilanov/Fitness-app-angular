1609ce1df42bd49b20053c0007d29aaa55a9943a
Boris Milanov <boris.mil.96@gmail.com>
2024-09-26 21:25:55 +0300

    R add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 
 import { PythonComponent } from './python/python.component';
+import { RComponent } from './r/r.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    PythonComponent
+    PythonComponent,
+    RComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/r/r.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RComponent } from './r.component';
+
+describe('RComponent', () => {
+  let component: RComponent;
+  let fixture: ComponentFixture<RComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RComponent]
+    });
+    fixture = TestBed.createComponent(RComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
