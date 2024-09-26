c3dbea4befdc548ac74af350a04597890933ce36
Boris Milanov <boris.mil.96@gmail.com>
2024-09-26 21:13:26 +0300

    Python info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms'; // Import this
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 
+import { PythonComponent } from './python/python.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    PythonComponent
   ],
   imports: [
     BrowserModule,
